5d191e7ac659c6c1f3c344c27d836eb39d890ba1
Mick Semb Wever <mck@apache.org>
2022-04-12 17:09:39 +0200

    tmp

--- jenkins-dsl/sync-ci-cassandra-webapges.sh
+++ jenkins-dsl/sync-ci-cassandra-webapges.sh
@@ -19,7 +19,7 @@ cd tmp-ci-cassandra.apache.org
 #for pipeline in "2.2" "3.0" "3.11" "4.0" "trunk" "devbranch" ; do
 #    for job_suffix in "" "-artifacts" "-cqlsh-tests" "-dtest" "-dtest-large" "-dtest-novnode" "-dtest-offheap" "-dtest-upgrade" "-fqltool-test" "-jvm-dtest" "-jvm-dtest-upgrade" "-long-test" "-stress-test" "-test" "-test-burn" "-test-cdc" "-test-compression" "-microbench" ; do
       for pipeline in "2.2"  ; do
-          for job_suffix in "" "-artifacts" ; do      
+          for job_suffix in "" ; do      
         echo "Searching Cassandra-${pipeline}${job_suffix}"
         latest_url="https://ci-cassandra.apache.org/job/Cassandra-${pipeline}${job_suffix}/lastSuccessfulBuild/api/json?tree=number"
         if curl --output /dev/null --silent --head --fail ${latest_url}; then
@@ -38,19 +38,16 @@ cd tmp-ci-cassandra.apache.org
             if (( ${latest} > ${latest_saved} )) ; then
                 for build_number in $(seq $((${latest_saved}+1)) ${latest}) ; do
                     main_url="https://ci-cassandra.apache.org/job/Cassandra-${pipeline}${job_suffix}/${build_number}/"
-                    if curl --output /dev/null --silent --head --fail "${main_url}/testReport/" ; then
-                        if [[ -z "$(find ci-cassandra.apache.org/job/Cassandra-${pipeline}${job_suffix}/ -name ${build_number})" ]] ; then
-                            wget -q -E -N -k -p $main_url
-                            if [[ $job_suffix -ne "-artifacts" ]] ; then
-                                wget -q -E -N -k -p ${main_url}/testReport/
-                                if test -f ci-cassandra.apache.org/job/Cassandra-${pipeline}${job_suffix}/${build_number}/testReport/index.html ; then
-                                    mkdir -p ci-cassandra.apache.org/blue/organizations/jenkins/Cassandra-${pipeline}${job_suffix}/detail/Cassandra-${pipeline}${job_suffix}/${build_number}/pipeline
-                                    cp ci-cassandra.apache.org/job/Cassandra-${pipeline}${job_suffix}/${build_number}/testReport/index.html ci-cassandra.apache.org/blue/organizations/jenkins/Cassandra-${pipeline}${job_suffix}/detail/Cassandra-${pipeline}${job_suffix}/${build_number}/pipeline/
-                                fi
-                            fi
-                            echo "Saved Cassandra-${pipeline}${job_suffix}/${build_number}"
+                    wget -q -E -N -k -p $main_url
+                    if [[ $job_suffix -ne "-artifacts" ]] ; then
+                        wget -q -E -N -k -p ${main_url}/testReport/
+                        if test -f ci-cassandra.apache.org/job/Cassandra-${pipeline}${job_suffix}/${build_number}/testReport ; then
+                            mkdir -p ci-cassandra.apache.org/blue/organizations/jenkins/Cassandra-${pipeline}${job_suffix}/detail/Cassandra-${pipeline}${job_suffix}/${build_number}/pipeline
+                            cp ci-cassandra.apache.org/job/Cassandra-${pipeline}${job_suffix}/${build_number}/testReport/index.html ci-cassandra.apache.org/blue/organizations/jenkins/Cassandra-${pipeline}${job_suffix}/detail/Cassandra-${pipeline}${job_suffix}/${build_number}/pipeline/
                         fi
                     fi
+                    echo "Saved Cassandra-${pipeline}${job_suffix}/${build_number}"
+                    exit 0
                 done
             fi
         fi
