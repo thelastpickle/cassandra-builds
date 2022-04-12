b31d8458b378ecfa7f6f5b07bcf3f9b5e7b22aca
Mick Semb Wever <mck@apache.org>
2022-04-12 17:26:38 +0200

    tmp

--- jenkins-dsl/sync-ci-cassandra-webapges.sh
+++ jenkins-dsl/sync-ci-cassandra-webapges.sh
@@ -19,7 +19,7 @@ cd tmp-ci-cassandra.apache.org
 #for pipeline in "2.2" "3.0" "3.11" "4.0" "trunk" "devbranch" ; do
 #    for job_suffix in "" "-artifacts" "-cqlsh-tests" "-dtest" "-dtest-large" "-dtest-novnode" "-dtest-offheap" "-dtest-upgrade" "-fqltool-test" "-jvm-dtest" "-jvm-dtest-upgrade" "-long-test" "-stress-test" "-test" "-test-burn" "-test-cdc" "-test-compression" "-microbench" ; do
       for pipeline in "2.2"  ; do
-          for job_suffix in "" ; do      
+          for job_suffix in "" "-artifacts" ; do      
         echo "Searching Cassandra-${pipeline}${job_suffix}"
         latest_url="https://ci-cassandra.apache.org/job/Cassandra-${pipeline}${job_suffix}/lastSuccessfulBuild/api/json?tree=number"
         if curl --output /dev/null --silent --head --fail ${latest_url}; then
@@ -47,7 +47,7 @@ cd tmp-ci-cassandra.apache.org
                         fi
                     #fi
                     echo "Saved Cassandra-${pipeline}${job_suffix}/${build_number}"
-                    exit 0
+                    #exit 0
                 done
             fi
         fi
